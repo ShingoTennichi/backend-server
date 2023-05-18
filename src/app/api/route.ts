26a3ad161df7b2b1bef1f2c8d1c2d9848342d2f9
Shingo Tennichi <sngsng.ttt@gmail.com>
2023-05-17 21:30:16 -0700

    added test api route

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,5 +1,12 @@
 // this is for testing
 
+// export async function GET() {
+//   return new Response("GET GET GET");
+// }
+
+import { prisma } from "../../../prisma/globalPrismaClient";
+
 export async function GET() {
-  return new Response("GET GET GET");
+  const data = await prisma.user.findMany();
+  return new Response(JSON.stringify({ data: data }));
 }
