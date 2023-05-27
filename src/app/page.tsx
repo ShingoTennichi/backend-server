5f5b0ea98b697f6b96133a3a1b7dbb650877341a
Shingo Tennichi <sngsng.ttt@gmail.com>
2023-05-27 01:33:51 -0700

    tested image component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main className="flex flex-col justify-center items-center min-h-screen gap-10">
-      <h1 className="text-4xl">Backend Server</h1>
+    <main className="flex flex-col justify-center items-center min-h-screen">
+      <div className="h-screen flex flex-col justify-center items-center">
+        <h1 className="font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Backend Server</h1>
+      </div>
+      {/* <div className="min-h-screen flex flex-col justify-center items-center">
+        <h2 className="text-4xl font-bold">Applications</h2>
+        <ul>
+          <li>
+            <Image src="/StockCharts.jpg" alt="Stock Carts" width="200" height="200" className="rounded-lg"></Image>
+          </li>
+        </ul>
+      </div> */}
     </main>
   );
 }
